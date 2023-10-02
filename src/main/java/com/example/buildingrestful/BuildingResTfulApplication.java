7fa85fea508f371f0177c029a518e3ec8edb9633
Himanshu_Garg <himanshug1609@gmail.com>
2023-10-02 17:49:08 +0530

    Test_checkin

---
+++ src/main/java/com/example/buildingrestful/BuildingResTfulApplication.java
@@ -0,0 +1,13 @@
+package com.example.buildingrestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildingResTfulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuildingResTfulApplication.class, args);
+    }
+
+}
